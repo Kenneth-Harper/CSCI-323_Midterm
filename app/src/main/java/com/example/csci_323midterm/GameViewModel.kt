741b5e6859448281b156922a142022f9743b7b7b
Kenneth-Harper <harperk845@gmail.com>
2023-10-17 15:57:07 -0400

    Created a dao for the database and added the dao as a construction parameter to the viewmodel

--- app/src/main/java/com/example/csci_323midterm/GameViewModel.kt
+++ app/src/main/java/com/example/csci_323midterm/GameViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GameViewModel : ViewModel()
+class GameViewModel(val dao: HighscoreDao) : ViewModel()
 {
     /**
      * @param _lastScore
---
+++ app/src/main/java/com/example/csci_323midterm/GameViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.csci_323midterm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class GameViewModelFactory(private val dao: HighscoreDao)
+    : ViewModelProvider.Factory
+{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T
+    {
+        if (modelClass.isAssignableFrom(GameViewModel::class.java))
+        {
+            return GameViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel")
+    }
+}
