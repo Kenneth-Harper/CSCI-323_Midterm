4f3590e2a3c1a5fd95847b195647b3b12f44d423
Kenneth-Harper <harperk845@gmail.com>
2023-10-17 00:19:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/csci_323midterm/MainFragment.kt
@@ -0,0 +1,28 @@
+package com.example.csci_323midterm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.csci_323midterm.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding : FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        return view
+    }
+
+
+}
---
+++ app/src/main/java/com/example/csci_323midterm/highscore.kt
@@ -0,0 +1,14 @@
+package com.example.csci_323midterm
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "highscore_table")
+data class Highscore(
+    @PrimaryKey(autoGenerate = true)
+    var scoreId: Long = 0L,
+    @ColumnInfo(name = "player_name")
+    var playerName: String="",
+    @ColumnInfo(name = "number_guesses")
+    var numberGuesses: Int=0)
